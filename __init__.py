a5cb8dfaf96757ea259e73046454cd2c89e7b3e6
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-06-27 22:59:35 +0800

    新增 【主页】标题。

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+root.title("海书面板-运维工具")
+root.geometry("800x600")
+
+title = Label(root, text="海书面板-运维工具", font=("Smiley Sans Oblique", 50))
+title.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+root.mainloop()
